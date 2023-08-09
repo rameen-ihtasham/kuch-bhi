41ebee3d581f3db4189b295daeeb168b72d67589
rameen-ihtasham <krameen500@gmail.com>
2023-08-09 17:45:50 +0500

    Added a file

---
+++ rameen.js
@@ -0,0 +1,11 @@
+let anas = 'chutiya'
+for(let i = 0 ; i<5 ;i++)
+{
+    let x = 1 ;
+    var y = 0 ;
+    console.log(x);
+    console.log(y);
+}
+console.log('out of loop')
+//console.log(x)
+console.log(y)
